777889120fdb0daf68e0888221ea313dda3bfc17
Oğuzhan <64753457+OguzhanUmutlu@users.noreply.github.com>
2021-09-06 18:08:40 +0300

    daha ii 👀

--- NameAgeVerification.py
+++ NameAgeVerification.py
@@ -1,58 +1,37 @@
-import time
+from time import sleep
+from datetime import date
 
-thisyear = 2021 # enter your current Example year thisyear = 2021
- 
-
-name = input("What is your name ")
-time.sleep(2)
-year = int(input("Enter your year of birth: "))
-year = thisyear - year
-time.sleep(2)
-print("Your name:", name, "Your age: ", year)
-time.sleep(2)
-print("System Check To Be Checked...")
-time.sleep(2)
-print("Connecting to the System.....")
-time.sleep(1)
-print("Connected successfully. ")
-
-time.sleep(2)
-print("Checking Name 5 seconds..")
-time.sleep(1)
-print("Checking Name 4 seconds..")
-time.sleep(1)
-print("Checking Name 3 seconds..")
-time.sleep(1)
-print("Checking Name 2 seconds..")
-time.sleep(1)
-print("Checking Name 1 seconds..")
-time.sleep(3)
+name = input("Enter your first name: ")
+sleep(2)
+age = date.today().year - int(input("Enter your year of birth: "))
+sleep(2)
+print("Your name: " + name + ", your age: " + str(age))
+for i in ["Arguments are going to be checked...", "Connecting to the system.....", "Connected successfully."]:
+    sleep(2)
+    print(i)
+sleep(1)
+for i in range(5):
+    sleep(1)
+    print("Checking name in " + str(5-i) + " seconds...")
+sleep(3)
 print("Successfully checked.")
 
-if (name=="İbrahim") or (name=="ibrahim"):
-    time.sleep(1)
-    print("Your Name Is Correct")
+sleep(1)
+if name == "İbrahim" or name == "ibrahim":
+    print("Your name is correct!")
 else:
-    time.sleep(1)
-    print("Name Wrong " +name)
+    print("The name " + name + " is wrong!")
 
-time.sleep(2)
-print("Age Checking 5 second..")
-time.sleep(1)
-print("Age Checking 4 second..")
-time.sleep(1)
-print("Age Checking 3 second..")
-time.sleep(1)
-print("Age Checking 2 second..")
-time.sleep(1)
-print("Age Checking 1 second..")
-time.sleep(3)
-print("Successfully checked..")
+sleep(1)
+for i in range(5):
+    sleep(1)
+    print("Age will be checked in " + str(5-i) + " seconds...")
+sleep(3)
+print("Successfully checked!")
 
-
-if (int(year)>=18 and int(year)<=25):
-    time.sleep(1)
-    print("You can pass. ")
+if 18 <= age <= 25:
+    sleep(1)
+    print("You can pass.")
 else:
-    time.sleep(1)
-    print("You can't get in!. ")
+    sleep(1)
+    print("You can't get in!")
